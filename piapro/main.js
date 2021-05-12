3302fe9d1240e5d308e9708f066285692b4be682
樱川流夏立 <1312552724@qq.com>
2021-05-12 20:14:41 +0800

    Update main.js

--- piapro/main.js
+++ piapro/main.js
@@ -4,7 +4,7 @@
 // @match       https://piapro.jp/
 // @grant       none
 // @grant       GM_addStyle
-// @version     1.0
+// @version     1.1
 // @updateURL   https://raw.githubusercontent.com/hunmer/Tampermonkey-Scripts/main/piapro/main.js
 // @author      neysummer2000
 // @description 2021/5/12下午4:16:43
@@ -60,53 +60,18 @@
         }
     }
 
-    function getTimeString(s) {
-        function _s(s, j = '') {
-            s = parseInt(s);
-            return (s == 0 ? '' : (s < 10 ? '0' + s : s) + j);
-        }
-
-        function _s1(s, j = '', d = '') {
-            s = parseInt(s);
-            return s < 10 ? '0' + s : s;
-        }
-
-        s = Number(s);
-        var h = 0,
-            m = 0;
-        if (s >= 3600) {
-            h = parseInt(s / 3600);
-            s %= 3600;
-        }
-        if (s >= 60) {
-            m = parseInt(s / 60);
-            s %= 60;
-        }
-        return '▶ [' + _s(h, ':') + _s(m, ':') + _s1(s, '') + '] ';
-    }
-
     function setActive(dom) {
         var actived = document.querySelector('._active');
         if (actived != undefined) {
             actived.classList.remove('_active');
         } else {
             unsafeWindow.audio.ontimeupdate = function() {
-                if (this.ended) {
-                    status = 'ended';
-                } else
-                if (this.paused) {
-                    status = 'paused';
-                } else
-                if (this.currentTime <= 0) {
-                    status = 'loading';
-                } else {
-                    status = getTimeString(this.duration - this.currentTime);
-                }
-                document.title = status;
+                if (this.currentTime > 0 && dom) document.title = '[' + parseInt(this.currentTime / this.duration * 100) + '%' + ']' + document.querySelector('._active').querySelector(isPC ? '.title' : '.over-txt').innerText;
             }
         }
         if (dom != undefined) {
             dom.classList.add('_active');
+            document.title = dom.querySelector(isPC ? '.title' : '.over-txt').innerText;
         }
     }
 
